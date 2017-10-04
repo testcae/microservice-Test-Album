7b060abc55e1ea5fa0b512c061e1138ab3818d7b
ukimiawz <deathmoon123>
2017-10-04 04:19:26 +0200

    Generated microservice version 1.0

---
+++ db/Test_Album_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  testMusic
+-- Automatically generated sql script for the service Test Album, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table testAlbum.
+--
+CREATE TABLE testMusic.testAlbum (
+  Title VARCHAR(255) ,
+  CoverImage VARCHAR(255) ,
+CONSTRAINT Title_PK PRIMARY KEY (Title)
+ 
+);
+
+
+
